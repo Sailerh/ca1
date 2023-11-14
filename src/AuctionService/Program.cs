dca18be269ee8363fca3778579b080b4010f5498
unknown <sailer@medien.at>
2023-11-15 00:20:07 +0100

    End of section 11

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,69 +1,86 @@
+using AuctionService;
 using AuctionService.Data;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<AuctionDbContext>(opt =>
+internal class Program
 {
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-}
-);
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-builder.Services.AddMassTransit(x => 
-{
-    x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+    private static void Main(string[] args)
     {
-        o.QueryDelay = TimeSpan.FromSeconds(10);
+        var builder = WebApplication.CreateBuilder(args);
 
-        o.UsePostgres();
-        o.UseBusOutbox();
-    });
+        // Add services to the container.
 
-    // x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
-    
-    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
-
-    x.UsingRabbitMq((context, cfg) =>
-    {
-        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host => 
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<AuctionDbContext>(opt =>
         {
-            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
-            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+            opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+        }
+        );
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        builder.Services.AddMassTransit(x =>
+        {
+            x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+            {
+                o.QueryDelay = TimeSpan.FromSeconds(10);
+
+                o.UsePostgres();
+                o.UseBusOutbox();
+            });
+
+            x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+
+            x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                // cfg.UseRetry(r =>
+                // {
+                //     r.Handle<RabbitMqConnectionException>();
+                //     r.Interval(5, TimeSpan.FromSeconds(10));
+                // });
+
+
+                cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+                {
+                    host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+                    host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+                });
+
+                cfg.ConfigureEndpoints(context);
+            });
         });
 
-        cfg.ConfigureEndpoints(context);
-    });
-});
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = builder.Configuration["IdentityServiceUrl"];
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters.ValidateAudience = false;
+                options.TokenValidationParameters.NameClaimType = "username";
+            });
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options => 
-    {
-        options.Authority = builder.Configuration["IdentityServiceUrl"];
-        options.RequireHttpsMetadata = false;
-        options.TokenValidationParameters.ValidateAudience = false;
-        options.TokenValidationParameters.NameClaimType = "username";
-    });
+        builder.Services.AddGrpc();
 
-var app = builder.Build();
+        var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-app.MapControllers();
+        app.MapControllers();
+        app.MapGrpcService<GrpcAuctionService>();
 
-try
-{
-    DbInitializer.InitDb(app);
+        try
+        {
+            DbInitializer.InitDb(app);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("from DbInitializer.InitDb() ex=" + ex);
+        }
+
+        app.Run();
+    }
 }
-catch(Exception ex)
-{
-    Console.WriteLine("from DbInitializer.InitDb() ex=" + ex);
-}
-
-app.Run();
